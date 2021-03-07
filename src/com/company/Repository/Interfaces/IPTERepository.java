e8595275be46bc7681a6e9e756f170679deb16ce
elmirazh <79574682+elmirazh@users.noreply.github.com>
2021-03-07 20:46:04 +0600

    Update IPTERepository.java

--- src/com/company/Repository/Interfaces/IPTERepository.java
+++ src/com/company/Repository/Interfaces/IPTERepository.java
@@ -6,8 +6,8 @@ import com.company.Entities.PartTimeEmployee;
 import java.util.List;
 
 public interface IPTERepository {
-    public boolean addPartTimeEmployee(PartTimeEmployee partTimeEmployee);
-    public List<PartTimeEmployee> getAllPartTimeEmployees();
-    public PartTimeEmployee getPTEByID(int id);
-    public double getPaymentPTEByID(int id);
+    public boolean addPartTimeEmployee(PartTimeEmployee partTimeEmployee); //for adding employee for part time
+    public List<PartTimeEmployee> getAllPartTimeEmployees(); //getting list of all part time employee
+    public PartTimeEmployee getPTEByID(int id); //searching part time employee by ID
+    public double getPaymentPTEByID(int id);//getting payment of part time employee by ID
 }
