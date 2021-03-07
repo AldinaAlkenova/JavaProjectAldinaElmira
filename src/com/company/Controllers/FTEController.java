1b0e8c3899b6b87c0feabd99690b8e25decee5ce
elmirazh <79574682+elmirazh@users.noreply.github.com>
2021-03-07 18:44:57 +0600

    Update FTEController.java

--- src/com/company/Controllers/FTEController.java
+++ src/com/company/Controllers/FTEController.java
@@ -1,12 +1,9 @@
-package com.company.Controllers;
+package com.company.controllers;
 
-import com.company.Entities.Employee;
 import com.company.Entities.FullTimeEmployee;
-import com.company.Repository.FTERepository;
-import com.company.Repository.Interfaces.IFTERepository;
+import com.company.repositories.interfaces.IFTERepository;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 public class FTEController {
@@ -15,18 +12,27 @@ public class FTEController {
     public FTEController(IFTERepository fteRepository) {
         this.fteRepository = fteRepository;
     }
-   public String addFullTimeEmployee (int employeeID, String name, String surname, int age, String position, double salary){
+
+    public String addFullTimeEmployee (int employeeID, String name, String surname, int age,
+                                       String position, double salary){
         LocalDate dateOfAgreement=LocalDate.now();
         LocalDate expireDate= LocalDate.now().plusYears(1);
-        FullTimeEmployee addFullTimeEmployee=new FullTimeEmployee(employeeID, name, surname, age, position, dateOfAgreement, expireDate, salary);
+        FullTimeEmployee addFullTimeEmployee=new FullTimeEmployee(employeeID, name, surname, age, position,
+                dateOfAgreement, expireDate, salary);
         boolean isAdded= fteRepository.addFullTimeEmployee(addFullTimeEmployee);
         if (isAdded=true){
             return "Completed!";
         }
         else return "Failed";
     }
+
     public List<FullTimeEmployee> getAllFullTimeEmployees() {
         List<FullTimeEmployee> fulltimeemployees = fteRepository.getAllFullTimeEmployees();
         return fulltimeemployees;
+    }
+
+    public String getFTEByID(int id){
+        String result=fteRepository.getFTEByID(id).toString();
+        return result;
     }
 }
