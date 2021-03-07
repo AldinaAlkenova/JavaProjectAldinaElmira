1b63bb5ce391df70291044586d1cd517f592c436
AldinaAlkenova <79828991+AldinaAlkenova@users.noreply.github.com>
2021-03-07 17:38:51 +0600

    Update FullTimeEmployee.java

--- src/com/company/Entities/FullTimeEmployee.java
+++ src/com/company/Entities/FullTimeEmployee.java
@@ -3,7 +3,7 @@ package com.company.Entities;
 import java.time.LocalDate;
 
 public class FullTimeEmployee extends Employee{
-    private double salary;
+    private static double salary;
     public FullTimeEmployee(int employeeID, String name, String surname, int age, String position, LocalDate dateofagreement, LocalDate expiredate, double salary) {
         super(employeeID, name, surname, age, position, dateofagreement, expiredate);
         this.salary=salary;
@@ -11,15 +11,22 @@ public class FullTimeEmployee extends Employee{
 
     @Override
     public double calculatePayment() {
-        return salary*12;
+        return 12*salary;
     }
 
     public FullTimeEmployee(){};
 
+    public static Double getSalary() {
+        return salary;
+    }
+
+    public static void setSalary(Double salary) {
+        FullTimeEmployee.salary = salary;
+    }
 
     @Override
     public String toString()
     {
-        return "Full time employee ID: " + super.getEmployeeID()+ "; name: " + super.getName() + "; surname: " + super.getSurname() + "; age: " + super.getAge() + "; position: " + getPosition();
+        return "Full time employee ID: " + super.getEmployeeID()+ "; name: " + super.getName() + "; surname: " + super.getSurname() + "; age: " + super.getAge() + "; position: " + getPosition() + "; salary: " + getSalary();
     }
 }
