de75468167b317eb7009e431babc42d5b8354d71
elmirazh <79574682+elmirazh@users.noreply.github.com>
2021-03-07 19:58:40 +0600

    Update Employee.java

--- src/com/company/Entities/Employee.java
+++ src/com/company/Entities/Employee.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.Date;
 
-public abstract class Employee {
+public abstract class Employee { //Abstract class Employee that has fields such as employeeID, age, name, surname, position, dateOfAgreement amd expireDate
     private static int employeeID;
     private static int age;
     private static String name;
@@ -13,8 +13,8 @@ public abstract class Employee {
     private static LocalDate dateOfAgreement;
     private static LocalDate expireDate;
 
-    public Employee(){};
-    public Employee(int employeeID, String name, String surname, int age, String position, LocalDate dateOfAgreement, LocalDate expireDate ) {
+    public Employee(){}; //default constructor
+    public Employee(int employeeID, String name, String surname, int age, String position, LocalDate dateOfAgreement, LocalDate expireDate ) {//parametrized constructor
         this.employeeID = employeeID;
         this.name = name;
         this.surname = surname;
@@ -23,60 +23,60 @@ public abstract class Employee {
         this.dateOfAgreement=dateOfAgreement;
         this.expireDate=expireDate;
     }
-    public abstract double calculatePayment ();
-    public static int getAge() {
+    public abstract double calculatePayment (); //abstract method "calculatePayment" that will calculate total salary
+    public static int getAge() { //getter for "age"
         return age;
     }
 
-    public static Integer getEmployeeID() {
+    public static Integer getEmployeeID() { //getter for "EmployeeID"
         return employeeID;
     }
 
-    public static String getName() {
+    public static String getName() { //getter for "name"
         return name;
     }
 
-    public static String getPosition() {
+    public static String getPosition() { //getter for "position"
         return position;
     }
 
-    public static String getSurname() {
+    public static String getSurname() { //getter for "surname"
         return surname;
     }
 
-    public static LocalDate getExpireDate() {
+    public static LocalDate getExpireDate() { //getter for "expireDate"
         return expireDate;
     }
 
-    public static LocalDate getDateOfAgreement() {
+    public static LocalDate getDateOfAgreement() { //getter for "DateOfAgreement"
         return dateOfAgreement;
     }
 
-    public void setAge(Integer age) {
+    public void setAge(Integer age) { //setter for "age"
         this.age = age;
     }
 
-    public void setEmployeeID(Integer employeeID) {
+    public void setEmployeeID(Integer employeeID) { //setter for "employeeID"
         this.employeeID = employeeID;
     }
 
-    public void setName(String name) {
+    public void setName(String name) { //setter for name
         this.name = name;
     }
 
-    public void setDateOfAgreement(LocalDate dateOfAgreement) {
+    public void setDateOfAgreement(LocalDate dateOfAgreement) { //setter for "DateOfAgreement"
         this.dateOfAgreement = dateOfAgreement;
     }
 
-    public void setSurname(String surname) {
+    public void setSurname(String surname) { //setter for "surname"
         this.surname = surname;
     }
 
-    public void setPosition(String position) {
+    public void setPosition(String position) { //setter for "position"
         this.position = position;
     }
 
-    public void setExpireDate(LocalDate expireDate) {
+    public void setExpireDate(LocalDate expireDate) { //setter for "expireDate"
         this.expireDate = expireDate;
     }
 }
