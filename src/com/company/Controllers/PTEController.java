c7bd687c1b7c54ae32158f05ff418eb766121ccd
AldinaAlkenova <79828991+AldinaAlkenova@users.noreply.github.com>
2021-03-07 23:15:16 +0600

    Update PTEController.java

--- src/com/company/Controllers/PTEController.java
+++ src/com/company/Controllers/PTEController.java
@@ -11,7 +11,7 @@ public class PTEController {
     public PTEController(IPTERepository pteRepository) {
         this.pteRepository = pteRepository;
     }
-    public String addPartTimeEmployee (int employeeID, String name, String surname, int age, String position, double hoursWorked, double rate){
+    public String addPartTimeEmployee (int employeeID, String name, String surname, int age, String position, Double hoursWorked, Double rate){
         LocalDate dateOfAgreement=LocalDate.now();
         LocalDate expireDate= LocalDate.now().plusYears(1);
         PartTimeEmployee addPartTimeEmployee=new PartTimeEmployee(employeeID, name, surname, age, position, dateOfAgreement, expireDate, hoursWorked, rate);
@@ -39,7 +39,7 @@ public class PTEController {
         else {
             return "The Employee ID was not found among Part Time Employees. Please, make sure you have entered correct information.";
         }}
-    public String updatePTEByID(int id, String name, String surname, int age, String position, double hoursWorked, double rate){
+    public String updatePTEByID(int id, String name, String surname, int age, String position, Double hoursWorked, Double rate){
         Boolean isUpdated=pteRepository.updatePTEByID(id, name, surname, age, position, hoursWorked, rate);
         if (isUpdated=true){
             return "Completed!";
