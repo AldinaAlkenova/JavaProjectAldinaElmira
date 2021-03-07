179a61040b96e0c5c4f17cb3b8746eb309dc9db8
AldinaAlkenova <79828991+AldinaAlkenova@users.noreply.github.com>
2021-03-07 23:34:39 +0600

    Update PTEController.java

--- src/com/company/Controllers/PTEController.java
+++ src/com/company/Controllers/PTEController.java
@@ -11,6 +11,7 @@ public class PTEController {
     public PTEController(IPTERepository pteRepository) {
         this.pteRepository = pteRepository;
     }
+    //Method for adding new Part Time Employees
     public String addPartTimeEmployee (int employeeID, String name, String surname, int age, String position, Double hoursWorked, Double rate){
         LocalDate dateOfAgreement=LocalDate.now();
         LocalDate expireDate= LocalDate.now().plusYears(1);
@@ -21,16 +22,17 @@ public class PTEController {
         }
         else return "Failed";
     }
-
+//Method for printing all the Part Time Employees
     public List<PartTimeEmployee> getAllPartTimeEmployees() {
         List<PartTimeEmployee> parttimeemployee = pteRepository.getAllPartTimeEmployees();
         return parttimeemployee;
     }
-
+//Method for finding the Part Time Employees by ID
     public String getPTEByID(int id){
         String result=pteRepository.getPTEByID(id);
         return result;
     }
+    //Method for finding the payment of the Part Time Employee by ID
     public String getPaymentPTEByID(int id){
         Double result=pteRepository.getPaymentPTEByID(id);
         if (result>0){
