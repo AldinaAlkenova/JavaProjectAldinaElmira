7e0fb33fd789279f1b8ac6ce1d1b614575cb1a24
elmirazh <79574682+elmirazh@users.noreply.github.com>
2021-03-07 18:46:50 +0600

    Update IFTERepository.java

--- src/com/company/Repository/Interfaces/IFTERepository.java
+++ src/com/company/Repository/Interfaces/IFTERepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 public interface IFTERepository {
     public boolean addFullTimeEmployee(FullTimeEmployee fullTimeEmployee);
     public List<FullTimeEmployee> getAllFullTimeEmployees();
+    public FullTimeEmployee getFTEByID(int id);
 }
